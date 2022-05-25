8d9cd0741b9b4d1a18971b358eb3bd98c00a81ed
razanaman <97729999+razanaman@users.noreply.github.com>
2022-05-25 10:53:27 +0200

    premier test d'intégration formulaire

---
+++ jee-app/src/main/webapp/WEB-INF/ressources/js/dashboard.js
@@ -0,0 +1,8 @@
+function profile_open(){
+    document.getElementById("compte-profile-ferme").style.display="block";
+}
+
+function profile_close(){
+    document.getElementById("compte-profile-ferme").style.display="none";
+}
+
