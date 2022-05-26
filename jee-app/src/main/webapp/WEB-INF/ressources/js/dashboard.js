8871f319dc54dbb42e1bb076a6b964e93c43c222
razanaman <97729999+razanaman@users.noreply.github.com>
2022-05-26 10:57:10 +0200

    formulaire souscription à jour

--- jee-app/src/main/webapp/WEB-INF/ressources/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-function profile_open(){
-    document.getElementById("compte-profile-ferme").style.display="block";
-}
-
-function profile_close(){
-    document.getElementById("compte-profile-ferme").style.display="none";
-}
-
