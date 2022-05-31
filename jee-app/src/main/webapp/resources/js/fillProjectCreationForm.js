dba2387d9ad99ad23481a8965498513c11441c33
Ganonddalff <m-ateusz@live.fr>
2022-05-31 19:07:21 +0200

    Ajout script de remplissage création project

---
+++ jee-app/src/main/webapp/resources/js/fillProjectCreationForm.js
@@ -0,0 +1,9 @@
+function fillProjectCreationForm(){
+    document.getElementById("createProjectForm:nameInput").value = "En JavaScript svp";
+    document.getElementById("createProjectForm:startDateInput").value = "09-06-2022";
+    document.getElementById("createProjectForm:finishDateInput").value = "16-06-2022";
+    document.getElementById("createProjectForm:locationInput").value = "paris";
+    document.getElementById("createProjectForm:raiseTargetInput").value = "600000";
+    document.getElementById("createProjectForm:projectTypeInput").value = "Création";
+    document.getElementById("createProjectForm:descriptionInput").value = "Ce projet vise à démontrer l'utilité d'un remplissage automatique de formulaire via JavaScript. Vos dons sont défiscalisables.";
+}
